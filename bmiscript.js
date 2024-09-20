7dcf5e4edbdb2b74cd9b1d45bb393e6113260e31
ArabatzisAntonis <arabatzisantonis@yahoo.com>
2024-09-20 15:47:15 +0300

    final changes

--- bmiscript.js
+++ bmiscript.js
@@ -1,44 +1,44 @@
 
 
-    'use strict';
+'use strict';
 
     
-    console.log('bmiscript.js loaded');
+//console.log('bmiscript.js loaded');
 
-    function calculateBMI(){
-        let height = parseFloat(document.getElementById('txtfldHeight').value / 100); 
-        let weight = parseFloat(document.getElementById('txtfldWeight').value);
+function calculateBMI(){
+    let height = parseFloat(document.getElementById('txtfldHeight').value / 100); 
+    let weight = parseFloat(document.getElementById('txtfldWeight').value);
+
+        
+//console.log('Height (m):', height);
+//console.log('Weight (kg):', weight);
 
 
-        console.log('Height (m):', height);
-        console.log('Weight (kg):', weight);
-
-
-        //checkin numbers
-        if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
-            alert("Please enter valid numbers for height and weight.");
-            return;
-        }
+//checkin numbers
+if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+    alert("Please enter valid numbers for height and weight.");
+    return;
+}
         
 
-        let result = weight / (height * height);
+let resultBmi = weight / (height * height);
         
-
-        result = Math.round(result * 10) / 10;
-        console.log('BMI:', result);
+//1 number after dot
+resultBmi = Math.round(resultBmi * 10) / 10;
+//console.log('BMI:', result);
         
-        if (result < 18.5) {
-            alert ("Underweight");
-        } else if (result >= 18.5 && result < 24.9) {
-            alert ("Normal weight");
-        } else if (result >= 25 && result < 29.9) {
-            alert ("Overweight");
-        } else if (result >= 30) {
-            alert ("Obesity");
-        }
+if (resultBmi < 18.5) {
+    alert ("Underweight");
+} else if (resultBmi >= 18.5 && resultBmi < 24.9) {
+    alert ("Normal weight");
+} else if (resultBmi >= 25 && resultBmi < 29.9) {
+    alert ("Overweight");
+} else if (resultBmi >= 30) {
+    alert ("Obesity");
+}
     
         
-    };
+};
 
 
 
